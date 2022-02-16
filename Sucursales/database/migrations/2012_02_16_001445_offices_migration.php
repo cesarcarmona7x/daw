afcae2c62d25d50a5cb6a7ab97bdc7a039df5d79
Cesar Carmona <cesrafcata@gmail.com>
2022-02-15 17:58:04 -0700

    Trabajo 15/02/2022

---
+++ Sucursales/database/migrations/2012_02_16_001445_offices_migration.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('offices',function(Blueprint $table){
+            $table->increments('id');
+            $table->integer('country_id')->unsigned();
+            $table->string('key');
+            $table->timestamps();
+            $table->foreign('country_id')->references('id')->on('countries');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+        Schema::dropIfExists('offices');
+    }
+};
